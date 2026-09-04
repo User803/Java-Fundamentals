1f9dace2e3a7cfc4d4109fb4b87df7eb3a23961e
User803 <jacksongachugu@gmail.com>
2026-09-04 12:16:33 +0300

    lesson 06

--- Main.java
+++ Main.java
@@ -31,10 +31,23 @@ public class Main {
 //        input.close();
 //        System.out.println(sentence.toUpperCase());
 
+//        Scanner input = new Scanner(System.in);
+//        String name = input.nextLine();
+//        int age = input.nextInt();
+//        input.close();
+//        System.out.println("Hi, " + name + "! You are " + age + " years old.");
+
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
-        int age = input.nextInt();
+        int number = input.nextInt();
         input.close();
-        System.out.println("Hi, " + name + "! You are " + age + " years old.");
+        fizzbuzz(number);
+    }
+
+    public static void fizzbuzz(int number){
+        if (number % 15 == 0) {System.out.println("FizzBuzz");}
+        else if (number % 3 == 0) {System.out.println("Fizz");}
+        else if (number % 5 == 0) System.out.println("Buzz");
+
+        else System.out.println(number);
     }
 }
