4390aecfe607edd895f7201c45de43272869d4fb
User803 <jacksongachugu@gmail.com>
2026-09-04 11:37:30 +0300

    lesson 02

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Print the greeting below.
-        System.out.println("Hello, Java!");
+//        System.out.println("Hello, Java!");
+
+        Scanner input = new Scanner(System.in);
+        int firstNum = input.nextInt();
+        int secondNum = input.nextInt();
+        input.close();
+        System.out.println(firstNum + secondNum);
     }
 }
